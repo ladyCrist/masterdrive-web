170a1b412794be8256430429ff074b0f94db8f73
ladyCrist <116976078+ladyCrist@users.noreply.github.com>
2023-07-05 17:01:14 -0500

    Add files via upload

---
+++ js/noticias.js
@@ -0,0 +1,185 @@
+let DATA_NEWS = [
+  {
+    id: 0,
+    name: "¿Sabes cómo se tramita la Licencia Electrónica?",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuovSVSBhRiv14lbsxrvr0nDbdrzQFdd_SRA&usqp=CAU",
+  },
+  {
+    id: 1,
+    name: "MTC Toma nuevas medidas para trámite de Licencia de Conducir: ¿Cuáles son?",
+    img: "https://muchosnegociosrentables.com/wp-content/uploads/2016/03/C%C3%B3mo-montar-una-escuela-de-manejo.jpg",
+  },
+  {
+    id: 2,
+    name: "Gore Callao Reduce el trámite para entrega de Brevetes de dos meses a 24 Horas",
+    img: "https://www.automovilclub.cl/wp-content/uploads/2021/10/curso-garantizado-sidebar.jpg",
+  },
+  {
+    id: 3,
+    name: "¿Necesitas revalidad tu Licencia, pero no reconocen tus huellas dactilares?",
+    img: "https://storage.googleapis.com/sigo-blog-production/2023/05/BLOGS-6-1.jpg",
+  },
+  {
+    id: 4,
+    name: "¿Cómo podemos protegernos y cuidar de nuestro auto en esta temporada de calor?",
+    img: "https://www.automovilclub.cl/wp-content/uploads/2021/10/curso-garantizado-sidebar.jpg",
+  },
+  {
+    id: 5,
+    name: "¿Ya vas a rendir tu examen de manejo? ¡En esto no puedes equivocarte!",
+    img: "https://www.pymis.com.pe/wp-content/uploads/2022/07/curso-manejo-defensivo-4x4-imagen.jpg",
+  },
+  {
+    id: 6,
+    name: "Conducir un auto en subidas y bajadas pueden ser igual de complicadas: ¿Por qué?",
+    img: "https://www.pymis.com.pe/wp-content/uploads/2022/07/curso-manejo-defensivo-4x4-imagen.jpg",
+  },
+  {
+    id: 7,
+    name: "El delantero francés podrá tener todo el dinero de… indispensable y común para cualquier ciudadano..",
+    img: "https://www.automovilclub.cl/wp-content/uploads/2021/10/curso-garantizado-sidebar.jpg",
+  },
+  {
+    id: 8,
+    name: "¿Cumplió 16 años y obtuvo su brevete? Entérate ¿Cómo lo hizo?",
+    img: "https://www.pymis.com.pe/wp-content/uploads/2022/07/curso-manejo-defensivo-4x4-imagen.jpg",
+  },
+  {
+    id: 9,
+    name: "¿Qué pasa si choco el auto que alquilé en el centro de exámenes?",
+    img: "https://storage.googleapis.com/sigo-blog-production/2023/05/BLOGS-6-1.jpg",
+  },
+  {
+    id: 10,
+    name: "¿Cuánto me van a costar los trámites oara obtener mi brevete?",
+    img: "https://muchosnegociosrentables.com/wp-content/uploads/2016/03/C%C3%B3mo-montar-una-escuela-de-manejo.jpg",
+  },
+  {
+    id: 11,
+    name: "¿Qué pasa si desapruebo 30 veces el examen de manejo?",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuovSVSBhRiv14lbsxrvr0nDbdrzQFdd_SRA&usqp=CAU",
+  },
+  {
+    id: 12,
+    name: "Conoce cómo funciona el Sistema de Licencias de Conducir por Puntos",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuovSVSBhRiv14lbsxrvr0nDbdrzQFdd_SRA&usqp=CAU",
+  },
+  {
+    id: 13,
+    name: "Nuevos límites de velocidad para vehículos entra en vigencia este 13 de enero",
+    img: "https://www.pymis.com.pe/wp-content/uploads/2022/07/curso-manejo-defensivo-4x4-imagen.jpg",
+  },
+  {
+    id: 14,
+    name: "¿Qué sanción se obtiene al conducir un vehiculo sin licencia de conducir?",
+    img: "https://storage.googleapis.com/sigo-blog-production/2023/05/BLOGS-6-1.jpg",
+  },
+  {
+    id: 15,
+    name: "Cronograma para revalidar la licencia de conducir 2022",
+    img: "https://muchosnegociosrentables.com/wp-content/uploads/2016/03/C%C3%B3mo-montar-una-escuela-de-manejo.jpg",
+  },
+  {
+    id: 16,
+    name: "¿Cómo obtener mi brevete a la primera?",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuovSVSBhRiv14lbsxrvr0nDbdrzQFdd_SRA&usqp=CAU",
+  },
+  {
+    id: 17,
+    name: "7 Consejos para reducir los accidentes de tráfico esta Navidad",
+    img: "https://muchosnegociosrentables.com/wp-content/uploads/2016/03/C%C3%B3mo-montar-una-escuela-de-manejo.jpg",
+  },
+  {
+    id: 18,
+    name: "Mira estas 6 recomendaciones para los conductores que irán al sur por estas fiestas",
+    img: "https://www.pymis.com.pe/wp-content/uploads/2022/07/curso-manejo-defensivo-4x4-imagen.jpg",
+  },
+  {
+    id: 19,
+    name: "Licencia de conducir: ¿hasta qué fecha se amplió su vigencia según el MTC?",
+    img: "https://www.automovilclub.cl/wp-content/uploads/2021/10/curso-garantizado-sidebar.jpg",
+  },
+  {
+    id: 20,
+    name: "Los bellos pueblos alrededor de Lima que puedes visitar en auto",
+    img: "https://www.automovilclub.cl/wp-content/uploads/2021/10/curso-garantizado-sidebar.jpg",
+  },
+  {
+    id: 21,
+    name: "Conducir en estado de ebriedad puede ser sancionad…la cancelación del brevete y una multa de S/ 4300",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuovSVSBhRiv14lbsxrvr0nDbdrzQFdd_SRA&usqp=CAU",
+  },
+  {
+    id: 22,
+    name: "Artista peruano usa una mototaxi para replicar un robot de Transformers",
+    img: "https://www.automovilclub.cl/wp-content/uploads/2021/10/curso-garantizado-sidebar.jpg",
+  },
+  {
+    id: 23,
+    name: "Ampliación de la vigencia de las licencias de conducir",
+    img: "https://www.automovilclub.cl/wp-content/uploads/2021/10/curso-garantizado-sidebar.jpg",
+  },
+  {
+    id: 24,
+    name: "Protege a sus niños en el coche: menores de 12 años en los asientos traseros",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuovSVSBhRiv14lbsxrvr0nDbdrzQFdd_SRA&usqp=CAU",
+  },
+  {
+    id: 25,
+    name: "Multas para ciclistas se aplicarán desde el 3 de setiembre",
+    img: "https://storage.googleapis.com/sigo-blog-production/2023/05/BLOGS-6-1.jpg",
+  },
+  {
+    id: 26,
+    name: "Ahora puedes cambiar tu brevete físico por una licencia electrónica",
+    img: "https://muchosnegociosrentables.com/wp-content/uploads/2016/03/C%C3%B3mo-montar-una-escuela-de-manejo.jpg",
+  },
+  {
+    id: 27,
+    name: "¿Tienes miedo a conducir? Técnicas para superarlo",
+    img: "https://muchosnegociosrentables.com/wp-content/uploads/2016/03/C%C3%B3mo-montar-una-escuela-de-manejo.jpg",
+  },
+  {
+    id: 28,
+    name: "¿Qué pasaría si cae un rayo en tu coche mientras conduces?",
+    img: "https://www.automovilclub.cl/wp-content/uploads/2021/10/curso-garantizado-sidebar.jpg",
+  },
+  {
+    id: 29,
+    name: "Mascotas: ¿Cómo conseguir que viajen tranquilos en el auto?",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuovSVSBhRiv14lbsxrvr0nDbdrzQFdd_SRA&usqp=CAU",
+  },
+  {
+    id: 30,
+    name: "¿Influye la música en la conducción?",
+    img: "https://muchosnegociosrentables.com/wp-content/uploads/2016/03/C%C3%B3mo-montar-una-escuela-de-manejo.jpg",
+  },
+  {
+    id: 31,
+    name: "Plataforma Virtual de Clases Teóricas 1",
+    img: "https://muchosnegociosrentables.com/wp-content/uploads/2016/03/C%C3%B3mo-montar-una-escuela-de-manejo.jpg",
+  },
+  {
+    id: 32,
+    name: "Bus Eco Free para traslados",
+    img: "https://www.pymis.com.pe/wp-content/uploads/2022/07/curso-manejo-defensivo-4x4-imagen.jpg",
+  },
+];
+
+let card_string = "";
+
+DATA_NEWS.forEach((el, index) => {
+  card_string += `
+    <div class="news_card">
+      <img
+        src="${el.img}"
+        alt="${index}"
+      />
+      <div class="title">
+        <h3>${el.name}</h3>
+      </div>
+    </div>
+  `;
+});
+
+document.querySelector(".news").innerHTML = card_string;
